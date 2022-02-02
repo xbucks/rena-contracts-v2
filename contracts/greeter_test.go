ad60265fc94a4a1a996b2f0ad7b8e349d63741c8
xbucks <jmy.kot@gmail.com>
2022-02-02 10:49:01 +0900

    Add greeter contract

---
+++ contracts/greeter_test.go
@@ -0,0 +1,38 @@
+package contracts
+
+import (
+	"testing"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/core"
+	"math/big"
+)
+
+// Test greeter contract gets deployed correctly
+func TestDeployGreeter(t *testing.T) {
+	// Setup simulated block chain
+	key, _ := crypto.GenerateKey()
+	auth := bind.NewKeyedTransactor(key)
+	alloc := make(core.GenesisAlloc)
+	alloc[auth.From] = core.GenesisAccount{Balance: big.NewInt(1000000000)}
+	blockchain := backends.NewSimulatedBackend(alloc)
+
+	// Deploy contract
+	address, _, _, err := DeployGreeter(
+		auth,
+		blockchain,
+		"Hello World",
+	)
+
+	// Commit all pending transactions
+	blockchain.Commit()
+	
+	if err != nil {
+		t.Fatalf("Failed to deploy the Greeter contract: %v", err)
+	}
+
+	if len(address.Bytes()) == 0 {
+		t.Error("Expected a valid deployment address. Received empty address byte array instead")
+	}
+}
